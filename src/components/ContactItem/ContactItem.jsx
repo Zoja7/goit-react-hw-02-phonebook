2586ede2d3fdf6c477977fe5d0401d5bc5f0c84f
Zoja7 <yaozonka@gmail.com>
2023-10-23 17:53:31 +0300

    commit_13

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,18 +3,17 @@ export default function ContactItem({ contact, handleDeleteContact }) {
   return (
     <li className={css.contactItem}>
       <span className={css.contactName}>{contact.name}:</span>
-      <span className={css.contactNumber}>{contact.number}</span>
-      <span>
-        <button
-          className={css.deleteButton}
-          type="button"
-          onClick={() => {
-            handleDeleteContact(contact.id);
-          }}
-        >
-          Delete
-        </button>
-      </span>
+      <span className={css.contactNumber}> {contact.number}</span>
+
+      <button
+        className={css.deleteButton}
+        type="button"
+        onClick={() => {
+          handleDeleteContact(contact.id);
+        }}
+      >
+        Delete
+      </button>
     </li>
   );
 }
